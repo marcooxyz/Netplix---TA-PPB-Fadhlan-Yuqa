ee794defcdfbf7bd9fac16ee6bdc929475ab4940
rui <151373427+dazoey@users.noreply.github.com>
2025-12-08 19:35:47 +0700

    u

--- frontend/src/pages/MovieDetails.js
+++ frontend/src/pages/MovieDetails.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getMovieDetails } from '../api/api'; // Ganti sesuai dengan API yang digunakan
-import './MovieDetail.css';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieDetails } from "../api/tmdb"; // Ganti sesuai dengan API yang digunakan
+import "./MovieDetail.css";
+//adw
 
 const MovieDetail = () => {
   const { movieId } = useParams(); // Mengambil movieId dari URL
@@ -18,18 +19,24 @@ const MovieDetail = () => {
   if (!movie) return <div>Loading...</div>;
 
   return (
-    <div className="movie-detail">
-      {/* Background Full */}
+    <div className="movie-detail-container">
       <div
         className="movie-detail-background"
         style={{
           backgroundImage: `url('https://image.tmdb.org/t/p/original${movie.backdrop_path}')`,
         }}
-      >
-        <div className="movie-detail-content">
-          <h1 className="movie-title">{movie.title}</h1>
-          <p className="movie-overview">{movie.overview}</p>
-          {/* Tambahkan informasi lainnya sesuai kebutuhan */}
+      />
+      <div className="movie-detail-content">
+        <h1 className="movie-title">{movie.title}</h1>
+        <p className="movie-overview">{movie.overview}</p>
+        <div className="movie-trailer">
+          <iframe
+            src="https://www.youtube.com/embed/BjkIOU5PhyQ?autoplay=1&mute=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            title="Movie Trailer"
+          ></iframe>
         </div>
       </div>
     </div>
